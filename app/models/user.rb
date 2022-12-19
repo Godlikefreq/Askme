571c564ebb9d321d4462062dc3dff2ec49323024
Godlikefreq <godlikefreq@gmail.com>
2022-12-19 14:03:06 +0300

    Fixed default_facorite_color + regexp + callback moved to private

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_validation :downcase_nickname, :set_default_color
-
   validates :email, presence: true, uniqueness: true,
             format: { with: /\A[\w]+@[\w]+.[\w]+.?[\w]+?\z/ }
   validates :nickname, length: { maximum: 40 }, uniqueness: true,
             format: { with: /\A\w+\z/ }
-  validates :favorite_color, format: { with: /\A#([[:xdigit:]]{3}){1,2}\z/ }
+  validates :favorite_color, format: { with: /\A#[[:xdigit:]]{3}{1,2}\z/ }
 
-  has_many :questions
+  has_many :questions, dependent: :delete_all
 
   def downcase_nickname
-    nickname.downcase!
+    nickname&.downcase!
   end
 
-  def set_default_color
-    self.favorite_color ||= "#370617"
-  end
+  private
+
+  before_validation :downcase_nickname
 end
--- db/migrate/20221216163012_add_favcolor_to_users.rb
+++ db/migrate/20221216163012_add_favcolor_to_users.rb
@@ -1,5 +1,5 @@
 class AddFavcolorToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :favcolor, :string
+    add_column :users, :favorite_color, :string, default: "#370617"
   end
 end

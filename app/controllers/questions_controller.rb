431e300d22db4e2d6b1d1dc65e790e22ecaa6033
Godlikefreq <godlikefreq@gmail.com>
2022-12-20 13:28:02 +0300

    Questions at start page sorted

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -49,7 +49,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :desc)
     @question = Question.new
   end
 

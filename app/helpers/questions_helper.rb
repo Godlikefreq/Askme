f648fc242408e01acd9b9f348006a9d72d1709ad
Godlikefreq <godlikefreq@gmail.com>
2023-01-07 13:35:55 +0300

    Fixed regex for helper method

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def render_with_hashtags(body)
-    body.gsub(/#[a-zA-Z0-9А-Яа-я]+/){ |word| link_to word, "/questions/hashtag/#{word.delete("#")}"}.html_safe
+    body.gsub(/#[\wА-Яа-я]+/){ |word| link_to word, "/questions/hashtag/#{word.delete("#")}"}.html_safe
   end
 end
